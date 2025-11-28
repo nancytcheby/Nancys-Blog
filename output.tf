d104231604e8244273b6bc5b9d0124fc0ccc2005
nancytcheby <nancytcheby@hotmail.com>
2025-11-27 21:31:50 -0500

    BLOG_DEPLOYMENT_TERRAFORM: Create target group for Nancy's Blog (v1.4)

--- output.tf
+++ output.tf
@@ -7,3 +7,13 @@ output "blog_db_identifier" {
   description = "RDS instance identifier for Nancy's Blog"
   value       = aws_db_instance.nancy_blog_db.id
 }
+
+output "blog_target_group_arn" {
+  description = "ARN of Nancy's Blog target group"
+  value       = aws_lb_target_group.blog_tg.arn
+}
+
+output "blog_target_group_name" {
+  description = "Name of Nancy's Blog target group"
+  value       = aws_lb_target_group.blog_tg.name
+}
